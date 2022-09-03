8b7c2a361a78a46fb993fddc2afd8459560fbedf
james <independente30108@gmail.com>
2022-09-04 04:39:01 +0700

    3

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,9 +3,9 @@ const authorization = require('../config/authorize')
 const router        = express.Router() // Router จะทำงานเกี่ยวกับระบบรับส่งข้อมูลทั้งหมด
 
 //router.use("/api/create_tale", require("./router_create_tale"))
-router.use("/", (req, res)=> {  
-    res.send('Hello World!')
-})
+// router.use("/", (req, res)=> {  
+//     res.send('Hello World!')
+// })
 router.use("/login", require('./login'))
 router.use("/api/product", authorization, require("./router_product"))
 router.use("/api/product_type", authorization, require("./router_product_type"))
